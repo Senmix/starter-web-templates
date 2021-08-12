e300989bc8cedf51d3deea952d8e416e330cd6b2
Mixail <misha.senc@mail.ru>
2021-08-12 18:02:35 +0300

    Added babel loader and changed structure gulpfile. Added funts folder to #src

--- gulpfile.js
+++ gulpfile.js
@@ -1,328 +1,275 @@
 //let replace = require('gulp-replace'); //.pipe(replace('bar', 'foo'))
-let { src, dest } = require("gulp");
-let fs = require('fs');
-let gulp = require("gulp");
-let browsersync = require("browser-sync").create();
-let autoprefixer = require("gulp-autoprefixer");
-let scss = require('gulp-sass')(require('sass'));
-let group_media = require("gulp-group-css-media-queries");
-let plumber = require("gulp-plumber");
-let del = require("del");
-let imagemin = require("gulp-imagemin");
-let uglify = require("gulp-uglify-es").default;
-let rename = require("gulp-rename");
-let fileinclude = require("gulp-file-include");
-let clean_css = require("gulp-clean-css");
-let newer = require('gulp-newer');
+const { src, dest, watch, series, parallel } = require("gulp");
+const autoprefixer = require("gulp-autoprefixer"),
+    babel = require("gulp-babel"),
+    browsersync = require("browser-sync").create(),
+    clean_css = require("gulp-clean-css"),
+    del = require("del"),
+    fileinclude = require("gulp-file-include"),
+    fonter = require("gulp-fonter"),
+    fs = require("fs"),
+    group_media = require("gulp-group-css-media-queries"),
+    imagemin = require("gulp-imagemin"),
+    include = require("gulp-include"),
+    newer = require("gulp-newer"),
+    path = require("path"),
+    plumber = require("gulp-plumber"),
+    rename = require("gulp-rename"),
+    scss = require("gulp-sass")(require("sass")),
+    ttf2woff = require("gulp-ttf2woff"),
+    ttf2woff2 = require("gulp-ttf2woff2"),
+    uglify = require("gulp-uglify-es").default,
+    version = require("gulp-version-number"),
+    watchFilesItem = require("gulp-watch"),
+    webp = require("imagemin-webp"),
+    webpcss = require("gulp-webpcss"),
+    webphtml = require("gulp-webp-html");
 
-let version = require('gulp-version-number');
-let watchFilesItem = require('gulp-watch');
+const project_name = path.basename(__dirname);
+const src_folder = "#src";
 
-let webp = require('imagemin-webp');
-let webpcss = require("gulp-webpcss");
-let webphtml = require('gulp-webp-html');
-
-let fonter = require('gulp-fonter');
-
-let ttf2woff = require('gulp-ttf2woff');
-let ttf2woff2 = require('gulp-ttf2woff2');
-
-let project_name = require("path").basename(__dirname);
-let src_folder = "#src";
-
-let path = {
-	build: {
-		html: project_name + "/",
-		js: project_name + "/js/",
-		css: project_name + "/css/",
-		images: project_name + "/img/",
-		fonts: project_name + "/fonts/",
-		json: project_name + "/json/"
-	},
-	src: {
-		favicon: src_folder + "/img/favicon.{jpg,png,svg,gif,ico,webp}",
-		html: [src_folder + "/**/*.html", "!" + src_folder + "/_*.html"],
-		js: [src_folder + "/js/app.js", src_folder + "/js/vendors.js"],
-		css: src_folder + "/scss/style.scss",
-		images: [src_folder + "/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}", "!**/favicon.*"],
-		fonts: src_folder + "/fonts/*.ttf",
-		json: src_folder + "/json/**/*.*"
-	},
-	watch: {
-		html: src_folder + "/**/*.html",
-		js: src_folder + "/**/*.js",
-		css: src_folder + "/scss/**/*.scss",
-		images: src_folder + "/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}",
-		json: src_folder + "/json/**/*.*"
-	},
-	clean: "./" + project_name + "/"
+let pathes = {
+    build: {
+        html: path.resolve(project_name),
+        js: path.resolve(project_name, "js"),
+        css: path.resolve(project_name, "css"),
+        images: path.resolve(project_name, "img"),
+        fonts: path.resolve(project_name, "fonts"),
+        json: path.resolve(project_name, "json"),
+    },
+    src: {
+        favicon: path.resolve(src_folder, "img", "favicon.{jpg,png,svg,gif,ico,webp}"),
+        html: [path.resolve(src_folder, "**", "*.html"), "!" + path.resolve(src_folder, "_*.html")],
+        js: [path.resolve(src_folder, "js", "app.js"), path.resolve(src_folder, "js", "vendors.js")],
+        css: path.resolve(src_folder, "scss", "style.scss"),
+        images: [
+            path.resolve(src_folder, "img", "**", "*.{jpg,jpeg,png,svg,gif,ico,webp}"),
+            path.resolve("!**", "favicon.*"),
+        ],
+        fonts: path.resolve(src_folder, "fonts*.ttf"),
+        json: path.resolve(src_folder, "json", "**", "*.*"),
+    },
+    watch: {
+        html: path.resolve(src_folder, "**", "*.html"),
+        js: path.resolve(src_folder, "**", "*.js"),
+        css: path.resolve(src_folder, "scss", "**", "*.scss"),
+        images: path.resolve(src_folder, "img", "**", "*.{jpg,jpeg,png,svg,gif,ico,webp}"),
+        json: path.resolve(src_folder, "json", "**", "*.*"),
+    },
+    clean: project_name,
 };
 
 // Пишем папки которые нужно копировать через запятую
-let foldersArray = ['videos']; // 'videos', 'files' ...
+let foldersArray = ["videos"]; // 'videos', 'files' ...
 function copyFolders() {
-	foldersArray.forEach(folderItem => {
-		src(src_folder + "/" + folderItem + "/**/*.*", {})
-			.pipe(newer(project_name + "/" + folderItem + "/"))
-			.pipe(dest(project_name + "/" + folderItem + "/"));
-	});
-	return src(path.src.html).pipe(browsersync.stream());
+    foldersArray.forEach((folder) => {
+        src(path.resolve(src_folder, folder, "**", "*.*"), {})
+            .pipe(newer(path.resolve(project_name, folder)))
+            .pipe(dest(path.resolve(project_name, folder)));
+    });
+    return src(pathes.src.html).pipe(browsersync.stream());
 }
-function browserSync(done) {
-	browsersync.init({
-		server: {
-			baseDir: "./" + project_name + "/"
-		},
-		notify: false,
-		port: 3000,
-	});
-}
-function html() {
-	return src(path.src.html, {})
-		.pipe(fileinclude())
-		.on('error', function (err) {
-			console.error('Error!', err.message);
-		})
-		.pipe(dest(path.build.html))
-		.pipe(browsersync.stream());
-}
-function css() {
-	return src(path.src.css, {})
-		.pipe(
-			scss({ outputStyle: 'expanded' }).on('error', scss.logError)
-		)
-		.pipe(
-			rename({
-				extname: ".min.css"
-			})
-		)
-		.pipe(dest(path.build.css))
-		.pipe(browsersync.stream());
-}
-function json() {
-	return src(path.src.json, {})
-		.pipe(dest(path.build.json))
-		.pipe(browsersync.stream());
-}
-function js() {
-	return src(path.src.js, {})
-		.pipe(fileinclude())
-		.on('error', function (err) {
-			console.error('Error!', err.message);
-		})
-		.pipe(
-			rename({
-				suffix: ".min",
-				extname: ".js"
-			})
-		)
-		.pipe(dest(path.build.js))
-		.pipe(browsersync.stream());
-}
-function images() {
-	return src(path.src.images)
-		.pipe(newer(path.build.images))
-		.pipe(dest(path.build.images))
-}
-function favicon() {
-	return src(path.src.favicon)
-		.pipe(plumber())
-		.pipe(
-			rename({
-				extname: ".ico"
-			})
-		)
-		.pipe(dest(path.build.html))
-}
-function fonts_otf() {
-	return src('./' + src_folder + '/fonts/*.otf')
-		.pipe(plumber())
-		.pipe(fonter({
-			formats: ['ttf']
-		}))
-		.pipe(gulp.dest('./' + src_folder + '/fonts/'));
-}
-function fonts() {
-	src(path.src.fonts)
-		.pipe(plumber())
-		.pipe(ttf2woff())
-		.pipe(dest(path.build.fonts));
-	return src(path.src.fonts)
-		.pipe(ttf2woff2())
-		.pipe(dest(path.build.fonts))
-		.pipe(browsersync.stream());
-}
-function fontstyle() {
-	let file_content = fs.readFileSync(src_folder + '/scss/fonts.scss');
-	if (file_content == '') {
-		fs.writeFile(src_folder + '/scss/fonts.scss', '', cb);
-		fs.readdir(path.build.fonts, function (err, items) {
-			if (items) {
-				let c_fontname;
-				for (var i = 0; i < items.length; i++) {
-					let fontname = items[i].split('.');
-					fontname = fontname[0];
-					if (c_fontname != fontname) {
-						fs.appendFile(src_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
-					}
-					c_fontname = fontname;
-				}
-			}
-		})
-	}
-	return src(path.src.html).pipe(browsersync.stream());
-}
-function infofile() {
 
+function browserSync(done) {
+    browsersync.init({
+        server: { baseDir: project_name },
+        notify: false,
+        port: 3000,
+    });
 }
-function cb() { }
+
+function html() {
+    return src(pathes.src.html, {})
+        .pipe(fileinclude({ prefix: "<!--=", suffix: "-->" }))
+        .on("error", (err) => console.error("Error!", err.message))
+        .pipe(dest(pathes.build.html))
+        .pipe(browsersync.stream());
+}
+
+function css() {
+    return src(pathes.src.css, {})
+        .pipe(scss({ outputStyle: "expanded" }).on("error", scss.logError))
+        .pipe(rename({ extname: ".min.css" }))
+        .pipe(dest(pathes.build.css))
+        .pipe(browsersync.stream());
+}
+
+function json() {
+    return src(pathes.src.json, {}).pipe(dest(pathes.build.json)).pipe(browsersync.stream());
+}
+
+function js() {
+    return (
+        src(pathes.src.js, {})
+            // .pipe(fileinclude())
+            .pipe(include())
+            .on("error", (err) => console.error("Error!", err.message))
+            .pipe(rename({ suffix: ".min", extname: ".js" }))
+            .pipe(dest(pathes.build.js))
+            .pipe(browsersync.stream())
+    );
+}
+
+function images() {
+    return src(pathes.src.images).pipe(newer(pathes.build.images)).pipe(dest(pathes.build.images));
+}
+
+function favicon() {
+    return src(pathes.src.favicon)
+        .pipe(plumber())
+        .pipe(rename({ extname: ".ico" }))
+        .pipe(dest(pathes.build.html));
+}
+
+function fonts_otf() {
+    return src(path.resolve(src_folder, "fonts", "*.otf"))
+        .pipe(plumber())
+        .pipe(fonter({ formats: ["ttf"] }))
+        .pipe(dest(path.resolve(src_folder, "fonts")));
+}
+
+function fonts() {
+    src(pathes.src.fonts).pipe(plumber()).pipe(ttf2woff()).pipe(dest(pathes.build.fonts));
+    return src(pathes.src.fonts).pipe(ttf2woff2()).pipe(dest(pathes.build.fonts)).pipe(browsersync.stream());
+}
+
+function fontstyle() {
+    let file_content = fs.readFileSync(path.resolve(src_folder, "scss", "fonts.scss"));
+    if (file_content == "") {
+        fs.writeFile(path.resolve(src_folder, "scss", "fonts.scss"), "", cb);
+        fs.readdir(pathes.build.fonts, (err, items) => {
+            if (items) {
+                let c_fontname;
+                for (var i = 0; i < items.length; i++) {
+                    let fontname = items[i].split(".");
+                    fontname = fontname[0];
+                    if (c_fontname != fontname) {
+                        fs.appendFile(
+                            path.resolve(src_folder, "scss", "fonts.scss"),
+                            `@include font("${fontname}", "${fontname}", "400", "normal");\r\n`,
+                            cb
+                        );
+                    }
+                    c_fontname = fontname;
+                }
+            }
+        });
+    }
+    return src(pathes.src.html).pipe(browsersync.stream());
+}
+
+function infofile() {}
+
+function cb() {}
 
 function clean() {
-	return del(path.clean);
+    return del(pathes.clean);
 }
+
 function watchFiles() {
-	/*
-	watchFilesItem([path.watch.css], function () {
-		css();
-	});
-	watchFilesItem([path.watch.html], function () {
-		html();
-	});
-	watchFilesItem([path.watch.js], function () {
-		js();
-	});
-	watchFilesItem([path.watch.images], function () {
-		images();
-	});
-	watchFilesItem([path.watch.json], function () {
-		json();
-	});
-	*/
-	gulp.watch([path.watch.html], html);
-	gulp.watch([path.watch.css], css);
-	gulp.watch([path.watch.js], js);
-	gulp.watch([path.watch.json], json);
-	gulp.watch([path.watch.images], images);
+    watch([pathes.watch.html], html);
+    watch([pathes.watch.css], css);
+    watch([pathes.watch.js], js);
+    watch([pathes.watch.json], json);
+    watch([pathes.watch.images], images);
 }
+
 function cssBuild() {
-	return src(path.src.css, {})
-		.pipe(plumber())
-		.pipe(
-			scss({ outputStyle: 'expanded' }).on('error', scss.logError)
-		)
-		.pipe(group_media())
-		.pipe(
-			autoprefixer({
-				grid: true,
-				overrideBrowserslist: ["last 5 versions"],
-				cascade: true
-			})
-		)
-		.pipe(webpcss(
-			{
-				webpClass: "._webp",
-				noWebpClass: "._no-webp"
-			}
-		))
-		.pipe(dest(path.build.css))
-		.pipe(clean_css())
-		.pipe(
-			rename({
-				extname: ".min.css"
-			})
-		)
-		.pipe(dest(path.build.css))
-		.pipe(browsersync.stream());
+    return src(pathes.src.css, {})
+        .pipe(plumber())
+        .pipe(scss({ outputStyle: "expanded" }).on("error", scss.logError))
+        .pipe(group_media())
+        .pipe(autoprefixer({ grid: true, overrideBrowserslist: ["last 5 versions"], cascade: true }))
+        .pipe(webpcss({ webpClass: "._webp", noWebpClass: "._no-webp" }))
+        .pipe(dest(pathes.build.css))
+        .pipe(clean_css())
+        .pipe(rename({ extname: ".min.css" }))
+        .pipe(dest(pathes.build.css))
+        .pipe(browsersync.stream());
 }
+
 function jsBuild() {
-	let appPath = path.build.js + 'app.min.js';
-	let vendorsPath = path.build.js + 'vendors.min.js';
-	del(appPath);
-	del(vendorsPath);
-	return src(path.src.js, {})
-		.pipe(plumber())
-		.pipe(fileinclude())
-		.pipe(gulp.dest(path.build.js))
-		.pipe(uglify(/* options */))
-		.on('error', function (err) { console.log(err.toString()); this.emit('end'); })
-		.pipe(
-			rename({
-				suffix: ".min",
-				extname: ".js"
-			})
-		)
-		.pipe(dest(path.build.js))
-		.pipe(browsersync.stream());
+    let appPath = path.resolve(pathes.build.js, "app.min.js");
+    let vendorsPath = path.resolve(pathes.build.js, "vendors.min.js");
+    del(appPath);
+    del(vendorsPath);
+    return src(pathes.src.js, {})
+        .pipe(plumber())
+        .pipe(include())
+        .pipe(dest(pathes.build.js))
+        .pipe(babel({ presets: ["@babel/env"] }))
+        .pipe(uglify(/* options */))
+        .on("error", (err) => {
+            console.log(err.toString());
+            this.emit("end");
+        })
+        .pipe(rename({ extname: ".min.js" }))
+        .pipe(dest(pathes.build.js))
+        .pipe(browsersync.stream());
 }
+
 function imagesBuild() {
-	return src(path.src.images)
-		//.pipe(newer(path.build.images))
-		.pipe(
-			imagemin([
-				webp({
-					quality: 85
-				})
-			])
-		)
-		.pipe(
-			rename({
-				extname: ".webp"
-			})
-		)
-		.pipe(dest(path.build.images))
-		.pipe(src(path.src.images))
-		//.pipe(newer(path.build.images))
-		.pipe(
-			imagemin({
-				progressive: true,
-				svgoPlugins: [{ removeViewBox: false }],
-				interlaced: true,
-				optimizationLevel: 3 // 0 to 7
-			})
-		)
-		.pipe(dest(path.build.images))
+    return (
+        src(pathes.src.images)
+            //.pipe(newer(pathes.build.images))
+            .pipe(imagemin([webp({ quality: 85 })]))
+            .pipe(rename({ extname: ".webp" }))
+            .pipe(dest(pathes.build.images))
+            .pipe(src(pathes.src.images))
+            //.pipe(newer(pathes.build.images))
+            .pipe(
+                imagemin({
+                    progressive: true,
+                    svgoPlugins: [{ removeViewBox: false }],
+                    interlaced: true,
+                    optimizationLevel: 3, // 0 to 7
+                })
+            )
+            .pipe(dest(pathes.build.images))
+    );
 }
+
 function htmlBuild() {
-	return src(path.src.html, {})
-		.pipe(plumber())
-		.pipe(fileinclude())
-		.pipe(webphtml())
-		.pipe(version({
-			'value': '%DT%',
-			'replaces': [
-				'#{VERSION_REPlACE}#',
-				[/#{VERSION_REPlACE}#/g, '%TS%']
-			],
-			'append': {
-				'key': '_v',
-				'cover': 0,
-				'to': [
-					'css',
-					['image', '%TS%'],
-					{
-						'type': 'js',
-						'attr': ['src', 'custom-src'], // String or Array, undefined this will use default. css: "href", js: ...
-						'key': '_v',
-						'value': '%DT%',
-						'cover': 1,
-						'files': ['app.min.js', 'vendors.min.js'] // Array [{String|Regex}] of explicit files to append to
-					}
-				]
-			},
-			'output': {
-				'file': 'version.json'
-			}
-		}))
-		.pipe(dest(path.build.html))
-		.pipe(browsersync.stream());
+    return src(pathes.src.html, {})
+        .pipe(plumber())
+        .pipe(fileinclude({ prefix: "<!--=", suffix: "-->" }))
+        .pipe(webphtml())
+        .pipe(
+            version({
+                value: "%DT%",
+                replaces: ["#{VERSION_REPlACE}#", [/#{VERSION_REPlACE}#/g, "%TS%"]],
+                append: {
+                    key: "_v",
+                    cover: 0,
+                    to: [
+                        "css",
+                        ["image", "%TS%"],
+                        {
+                            type: "js",
+                            attr: ["src", "custom-src"], // String or Array, undefined this will use default. css: "href", js: ...
+                            key: "_v",
+                            value: "%DT%",
+                            cover: 1,
+                            files: ["app.min.js", "vendors.min.js"], // Array [{String|Regex}] of explicit files to append to
+                        },
+                    ],
+                },
+                output: {
+                    file: "version.json",
+                },
+            })
+        )
+        .pipe(dest(pathes.build.html))
+        .pipe(browsersync.stream());
 }
-let fontsBuild = gulp.series(fonts_otf, fonts, fontstyle);
-let buildDev = gulp.series(clean, gulp.parallel(fontsBuild, copyFolders, json, html, css, js, favicon, images));
-let watch = gulp.series(buildDev, gulp.parallel(watchFiles, browserSync));
-let build = gulp.parallel(htmlBuild, cssBuild, jsBuild, imagesBuild);
+
+let fontsBuild = series(fonts_otf, fonts, fontstyle);
+let buildDev = series(clean, parallel(fontsBuild, copyFolders, json, html, css, js, favicon, images));
+let watchDev = series(buildDev, parallel(watchFiles, browserSync));
+let build = series(clean, parallel(htmlBuild, cssBuild, jsBuild, imagesBuild));
 
 exports.copy = copyFolders;
 exports.fonts = fontsBuild;
 exports.build = build;
-exports.watch = watch;
-exports.default = watch;
+exports.watch = watchDev;
+exports.default = watchDev;
